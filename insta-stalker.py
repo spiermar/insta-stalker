253eaa41b15baa2a98d31198bebe1feffa114335
Martin Spier <spiermar@gmail.com>
2013-05-31 09:36:51 -0700

    Fixing imports and function definition.

--- insta-stalker.py
+++ insta-stalker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import sysm getopt
+import sys
+import getopt
 import calendar
 import urlparse
 import ConfigParser
@@ -9,37 +10,37 @@ from instagram.client import InstagramAPI
 from instagram.helper import datetime_to_timestamp
 from instagram.helper import timestamp_to_datetime
 
-def get_tag_media(tag_name, from_time, to_time)
-config = ConfigParser.ConfigParser()
-config.read('insta-stalker.cfg')
+def get_tag_media(tag_name, from_time, to_time):
+	config = ConfigParser.ConfigParser()
+	config.read('insta-stalker.cfg')
 
-access_token = config.get('insta-stalker', 'access_token')
-api = InstagramAPI(access_token=access_token)
+	access_token = config.get('insta-stalker', 'access_token')
+	api = InstagramAPI(access_token=access_token)
 
-count = 20
+	count = 20
 
-print 'from_time: ' + str(timestamp_to_datetime(from_time / 1000))
-print 'to_time: ' + str(timestamp_to_datetime(to_time / 1000))
+	print 'from_time: ' + str(timestamp_to_datetime(from_time / 1000))
+	print 'to_time: ' + str(timestamp_to_datetime(to_time / 1000))
 
-max_id = to_time
-while (max_id > from_time):
+	max_id = to_time
+	while (max_id > from_time):
 
-	recent_media, next = api.tag_recent_media(count, max_id, tag_name)
+		recent_media, next = api.tag_recent_media(count, max_id, tag_name)
 
-	# print 'count: ' + str(len(recent_media))
+		# print 'count: ' + str(len(recent_media))
 
-	for media in recent_media:
-		# print 'username: ' + media.user.username
-		# print 'link: ' + media.link
-		print 'time: ' + str(media.created_time)
-		# print 'id: ' + media.id
-		# print 'url: ' + media.images['standard_resolution'].url
+		for media in recent_media:
+			# print 'username: ' + media.user.username
+			# print 'link: ' + media.link
+			print 'time: ' + str(media.created_time)
+			# print 'id: ' + media.id
+			# print 'url: ' + media.images['standard_resolution'].url
 
-	parsednext = urlparse.urlparse(next)
+		parsednext = urlparse.urlparse(next)
 
-	max_id = int(urlparse.parse_qs(parsednext.query)['max_tag_id'][0])
+		max_id = int(urlparse.parse_qs(parsednext.query)['max_tag_id'][0])
 
-	print 'max_tag_id: ' + str(max_id) + ' (' + str(timestamp_to_datetime(max_id / 1000)) + ')'
+		print 'max_tag_id: ' + str(max_id) + ' (' + str(timestamp_to_datetime(max_id / 1000)) + ')'
 
 def main(argv):
 	tag_name = 'encorebeachclub'
